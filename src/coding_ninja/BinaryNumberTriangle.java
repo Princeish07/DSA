b21b021722af5b10074a4e3d324828265ed58990
prince <prince@infostride.com>
2023-10-27 11:11:41 +0530

    Date:- 27-Oct-2023 | CodingNinja-> Number Crown

--- src/coding_ninja/BinaryNumberTriangle.java
+++ src/coding_ninja/BinaryNumberTriangle.java
@@ -7,19 +7,18 @@ package coding_ninja;
 public class BinaryNumberTriangle {
     public static void nForest(int n) {
         int start = 1;
-        for(int i=0;i<n;i++){
-            if(i%2==0){
+        for (int i = 0; i < n; i++) {
+            if (i % 2 == 0) {
                 start = 1;
-            }else{
-                 start = 0;
+            } else {
+                start = 0;
             }
-            for(int j=0;j<=i;j++){
+            for (int j = 0; j <= i; j++) {
                 System.out.print(start);
-                    start = 1 - start;
+                start = 1 - start;
 
             }
             System.out.println();
-
 
 
         }
---
+++ src/coding_ninja/NumberCrown.java
@@ -0,0 +1,29 @@
+package coding_ninja;
+
+/**
+ * Date:- 27-10-2023
+ * NumberCrown
+ */
+public class NumberCrown {
+    public static void solution(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            for(int k=1;k<=2*(n-i);k++){
+                System.out.print("* ");
+            }
+            for(int s=i;s>=1;s--){
+                System.out.print(s+" ");
+            }
+            System.out.println();
+
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        solution(3);
+    }
+}
