dcdfcf7d19091881ed62f2c1b8f52f8e2022eee5
Prince-45963 <prince@infostride.com>
2023-11-04 09:56:20 +0530

    Coding Ninja | Date -> 4-nov-2023 | Butterfly Pattern

--- src/coding_ninja/RotatedTriangle.java
+++ src/coding_ninja/RotatedTriangle.java
@@ -4,14 +4,14 @@ public class RotatedTriangle {
     public static void nForest(int n) {
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
 
         }
         for(int i=n-1;i>0;i--){
             for(int j=1;j<=i;j++){
-                System.out.print("*");
+                System.out.print("* ");
             }
             System.out.println();
 
